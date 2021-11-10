88dc1792e5e8a8dd899632893c182b0c37ca2874
ycwhencpp <anurag.jay1002@gmail.com>
2021-11-10 18:47:49 +0530

    fixed ui

--- static/data.js
+++ static/data.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
+$(document).ready(function () {
+  $("#myInput").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#myTable tbody tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    $("#myTable tbody tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
 });
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "en",
+    },
+    "google_translate_element"
+  );
+}
